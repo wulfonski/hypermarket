ea66a0ef4f5a593d32bda6023c02953060e1a58b
Victor Corobceanu <victor.corobceanu@yahoo.ro>
2019-03-09 14:00:04 +0200

    Employee class

---
+++ hypermarket-core/src/main/java/ro/sda/hypermarket/core/entity/Employee.java
@@ -0,0 +1,101 @@
+package ro.sda.hypermarket.core.entity;
+
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "employees", schema = "hypermarket")
+
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="first_name", length = 40, nullable = false)
+    private String firstName;
+    @Column(name="last_name", length = 40, nullable = false)
+    private String lastName;
+    @Column(name = "salary", length = 10, nullable = false)
+    private double salary;
+    @Column(name = "job_title", length = 40, nullable = false)
+    private String jobTitle;
+
+
+    @Column(name = "city", length = 20, nullable = false)
+    private String city;
+    @ManyToOne(cascade={CascadeType.MERGE})
+    @JoinColumn(name="manager_id")
+    private Employee manager;
+
+    @OneToMany(mappedBy="manager")
+    private Set<Employee> subordinates = new HashSet<>();
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Employee getManager() {
+        return manager;
+    }
+
+    public void setManager(Employee manager) {
+        this.manager = manager;
+    }
+
+    public Set<Employee> getSubordinates() {
+        return subordinates;
+    }
+
+    public void setSubordinates(Set<Employee> subordinates) {
+        this.subordinates = subordinates;
+    }
+}
