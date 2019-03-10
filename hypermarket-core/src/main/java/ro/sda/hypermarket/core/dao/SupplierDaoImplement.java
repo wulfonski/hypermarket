555dc486e1a15c81bbbda7e03bc403e78d2374e4
wulfoski <Iosif*farcas88>
2019-03-10 12:10:47 +0200

    get All method

--- hypermarket-core/src/main/java/ro/sda/hypermarket/core/dao/SupplierDaoImplement.java
+++ hypermarket-core/src/main/java/ro/sda/hypermarket/core/dao/SupplierDaoImplement.java
@@ -1,14 +1,20 @@
 package ro.sda.hypermarket.core.dao;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.cache.spi.support.AbstractReadWriteAccess;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import ro.sda.hypermarket.core.entity.Supplier;
 
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +22,7 @@ import java.util.List;
 @Repository
 @Transactional
 public class SupplierDaoImplement implements SupplierDao {
+    EntityManager em;
 
     @Autowired
     private SessionFactory sessionFactory;
@@ -28,13 +35,12 @@ public class SupplierDaoImplement implements SupplierDao {
     }
 
     @Override
-    public List<Supplier> getAllSuppliers(){
-        Query query = sessionFactory.getCurrentSession().createQuery("FROM ro.sda.hypermarket.core.entity.Supplier");
-        List<Supplier> allSuppliers = query.getResultList();
+    public List<Supplier> getAllSuppliers() {
+        CriteriaQuery<Supplier> criteriaQuery = sessionFactory.getCurrentSession().getCriteriaBuilder().createQuery(Supplier.class);
+        criteriaQuery.from(Supplier.class);
+        List<Supplier> allSuppliers = sessionFactory.getCurrentSession().createQuery(criteriaQuery).getResultList();
         return allSuppliers;
-        
 
     }
-
 
 }
